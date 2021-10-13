d37e771d396f5bf56b04b180f4c4f59a56a5e199
Diego Alvarez <diego.canada@icloud.com>
2021-10-13 09:45:56 -0700

    fix fd binary name usage

--- diego-project.el
+++ diego-project.el
@@ -24,21 +24,22 @@
 
 ;;; Code:
 
-(setq diego-project-project-roots '(
-          "~/src/github.com/Shopify"
-          "~/code/"
-          "~/dotfiles/"))
+(require 'project)
+
+(defvar diego--project-project-roots
+  '("~/src/github.com/Shopify"
+    "~/code/"
+    "~/dotfiles/"))
 
 ;; Copied from Manuel Uberti:
 ;; <https://www.manueluberti.eu/emacs/2020/11/14/extending-project/>.
-;;
 (cl-defmethod project-root ((project (head local)))
   "Project root for PROJECT with HEAD and LOCAL."
   (cdr project))
 
 (defun mu--project-files-in-directory (dir)
   "Use `fd' to list files in DIR."
-    (unless (executable-find "fds")
+  (unless (executable-find "fd")
     (error "Cannot find 'fd' command is shell environment $PATH"))
   (let* ((default-directory dir)
          (localdir (file-local-name (expand-file-name dir)))
@@ -50,13 +51,12 @@
 ;;;###autoload
 (cl-defmethod project-files ((project (head local)) &optional dirs)
   "Override `project-files' to use `fd' in local projects."
-  (message ">>>>>>>>>>> oe")
   (mapcan #'mu--project-files-in-directory
           (or dirs (list (project-root project)))))
 
 (defun prot-project--list-projects ()
   "Produce list of projects in `prot-project-project-roots'."
-  (let* ((dirs diego-project-project-roots )
+  (let* ((dirs diego--project-project-roots )
          (dotless directory-files-no-dot-files-regexp)
          (cands (mapcan (lambda (d)
                           (directory-files d t dotless))
