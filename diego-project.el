5e2071eef11eea9782af414468cc303c652d07e2
Diego Alvarez <diego.canada@icloud.com>
2022-02-16 09:57:04 -0800

    remove unused methods

--- diego-project.el
+++ diego-project.el
@@ -28,29 +28,6 @@
 
 (defvar diego--project-project-roots '("~/src/github.com/Shopify" "~/code/oss"))
 
-;; Copied from Manuel Uberti:
-;; <https://www.manueluberti.eu/emacs/2020/11/14/extending-project/>.
-(cl-defmethod project-root ((project (head local)))
-  "Project root for PROJECT with HEAD and LOCAL."
-  (cdr project))
-
-(defun mu--project-files-in-directory (dir)
-  "Use `fd' to list files in DIR."
-  (unless (executable-find "fd")
-    (error "Cannot find 'fd' command is shell environment $PATH"))
-  (let* ((default-directory dir)
-         (localdir (file-local-name (expand-file-name dir)))
-         (command (format "fdx -t f -0 . %s" localdir)))
-    (project--remote-file-names
-     (sort (split-string (shell-command-to-string command) "\0" t)
-           #'string<))))
-
-;;;###autoload
-(cl-defmethod project-files ((project (head local)) &optional dirs)
-  "Override `project-files' to use `fd' in local projects."
-  (mapcan #'mu--project-files-in-directory
-          (or dirs (list (project-root project)))))
-
 (defun prot-project--list-projects ()
   "Produce list of projects in `prot-project-project-roots'."
   (let* ((dirs diego--project-project-roots )
