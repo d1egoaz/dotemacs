5a618c6cd0eb59c8b100f80dc5b222732ca3a1a5
Diego Alvarez <diego.canada@icloud.com>
2023-02-22 16:55:17 -0500

    Hello PragmataPro!!

--- modules/diego-ui-font.el
+++ modules/diego-ui-font.el
@@ -3,12 +3,13 @@
   :config
   ;; Fontconfig pattern, fontname[-fontsize][:name1=values1][:name2=values2]...
   ;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Fonts.html
-  (add-to-list 'default-frame-alist '(font . "Iosevka SS08-14:weight=medium"))
+  ;; (add-to-list 'default-frame-alist '(font . "Essential PragmataPro-14:weight=regular"))
+  (add-to-list 'default-frame-alist '(font . "Essential PragmataPro-14"))
 
   (setq-default line-spacing 1) ; needs to be changed for some fonts
 
   (set-face-attribute 'fixed-pitch nil
-                      :font "-*-Iosevka SS08-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+                      :font "-*-Essential PragmataPro-regular-normal-normal-*-14-*-*-*-m-0-iso10646-1")
   (set-face-attribute 'variable-pitch nil
                       :font "SF Pro Text-14")
   (set-face-attribute 'font-lock-comment-face nil
@@ -30,6 +31,6 @@
 (use-package mixed-pitch
   :hook (
          (org-mode-hook       . mixed-pitch-mode)
-         (gfm-mode-hook        . mixed-pitch-mode)))
+         (gfm-mode-hook       . mixed-pitch-mode)))
 
 (provide 'diego-ui-font)
