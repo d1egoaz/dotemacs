2588822a7e21fbfebf845742635d86f910881169
Diego Alvarez <diego.canada@icloud.com>
2023-03-03 14:27:28 -0800

    put result in a buffer as same as the query

--- modules/diego-chatgpt.el
+++ modules/diego-chatgpt.el
@@ -1,8 +1,18 @@
 (use-package request)
 (use-package json)
 
+(defun chatgpt-append-result (str)
+  "Insert result STR of the chatgpt-query at the end of buffer *ChatGPT*."
+  (let ((buf (get-buffer-create "*ChatGPT*")))
+    (with-current-buffer buf
+      (setq buffer-read-only nil)
+      (goto-char (point-max))
+      (insert (concat "\n" str))
+      (setq buffer-read-only t))
+    (pop-to-buffer buf))))
+
 (defun chatgpt-query (input)
-  "Interact with the ChatGPT API and return the response."
+  "Interact with the ChatGPT API with the INPUT and show the response."
   (let* ((api-key (diego/auth-source-get-password "api.openai.com" "apikey"))
          (url "https://api.openai.com/v1/chat/completions")
          (model "gpt-3.5-turbo")
@@ -17,9 +27,9 @@
                      :parser 'json-read
                      :success (cl-function
                                (lambda (&key data &allow-other-keys)
-                                 (message "🤖%s" (let ((message-content (aref (cdr (assoc 'choices data)) 0)))
-                                                   (cdr (assoc 'content (cdr (assoc 'message message-content)))))))))))
-    (message "❓%s\n" input)))
+                                 (chatgpt-append-result (format "🤖%s" (let ((message-content (aref (cdr (assoc 'choices data)) 0)))
+                                                                         (cdr (assoc 'content (cdr (assoc 'message message-content))))))))))))
+    (chatgpt-append-result (format "-----\n❓%s: %s" (format-time-string "%H:%M:%S PT") input))))
 
 (defun chatgpt ()
   "Interact with the ChatGPT API and display the response."
