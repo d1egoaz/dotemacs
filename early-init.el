175fceb6f77c8252fcc40064462d3249c43a79e5
Diego Alvarez <diego.canada@icloud.com>
2023-05-28 15:47:35 -0700

    update straight repo

--- early-init.el
+++ early-init.el
@@ -72,15 +72,14 @@
 ;; 'https is the default
 (setq straight-vc-git-default-protocol 'https)
 
-;; Following are standard setup lines from https://github.com/raxod502/straight.el.git
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
