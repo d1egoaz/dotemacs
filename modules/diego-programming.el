322b3cf2f411a6c0784d8c7814128846065eac2c
Diego Alvarez <info@diegoa.ca>
2024-04-09 16:08:54 -0700

    additional cape commands

--- modules/diego-programming.el
+++ modules/diego-programming.el
@@ -12,25 +12,29 @@
 ;; Add extensions
 (use-package cape
   ;; Bind dedicated completion commands
-  :bind
-  (("C-c p p" . completion-at-point) ;; capf
-   ("C-c p t" . complete-tag) ;; etags
-   ("C-c p d" . cape-dabbrev) ;; or dabbrev-completion
-   ("C-c p f" . cape-file)
-   ("C-c p k" . cape-keyword)
-   ("C-c p s" . cape-symbol)
-   ("C-c p a" . cape-abbrev)
-   ("C-c p i" . cape-ispell)
-   ("C-c p l" . cape-line)
-   ("C-c p w" . cape-dict)
-   ("C-c p \\" . cape-tex)
-   ("C-c p &" . cape-sgml)
-   ("C-c p r" . cape-rfc1345))
+  :bind (("C-c p p" . completion-at-point) ;; capf
+         ("C-c p t" . complete-tag)        ;; etags
+         ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
+         ("C-c p h" . cape-history)
+         ("C-c p f" . cape-file)
+         ("C-c p k" . cape-keyword)
+         ("C-c p s" . cape-elisp-symbol)
+         ("C-c p e" . cape-elisp-block)
+         ("C-c p a" . cape-abbrev)
+         ("C-c p l" . cape-line)
+         ("C-c p w" . cape-dict)
+         ("C-c p :" . cape-emoji)
+         ("C-c p \\" . cape-tex)
+         ("C-c p _" . cape-tex)
+         ("C-c p ^" . cape-tex)
+         ("C-c p &" . cape-sgml)
+         ("C-c p r" . cape-rfc1345))
   :init
   ;; Add `completion-at-point-functions', used by `completion-at-point'.
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
   (add-to-list 'completion-at-point-functions #'cape-file)
   (add-to-list 'completion-at-point-functions #'cape-tex)
-  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+   (add-to-list 'completion-at-point-functions #'cape-elisp-block)
   (add-to-list 'completion-at-point-functions #'cape-keyword))
 
 (use-package lsp-mode
