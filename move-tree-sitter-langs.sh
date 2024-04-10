7a428c76484547b3d6aa184fe521833b3898de5c
Diego Alvarez <info@diegoa.ca>
2024-04-10 15:27:57 -0700

    script to download and setup treesit

---
+++ move-tree-sitter-langs.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# M-x tree-sitter-langs-install-latest-grammar
+# then: ./move-tree-sitter-langs.sh
+
+for file in ~/.emacs.d/straight/build-30.0.50/tree-sitter-langs/bin/*.dylib; do
+  cp -f "$file" ~/.emacs.d/tree-sitter/libtree-sitter-$(basename "$file")
+  echo "File: libtree-sitter-$(basename "$file") copied!"
+done
