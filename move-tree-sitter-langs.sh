ee8fff0defd2b8aa60b497fdb2f0c71bda93bc02
Diego Alvarez <info@diegoa.ca>
2024-10-04 13:49:20 -0700

    update treesit install command

--- move-tree-sitter-langs.sh
+++ move-tree-sitter-langs.sh
@@ -3,7 +3,7 @@
 # M-x tree-sitter-langs-install-latest-grammar
 # then: ./move-tree-sitter-langs.sh
 
-for file in ~/.emacs.d/straight/build-30.0.50/tree-sitter-langs/bin/*.dylib; do
+for file in ~/.emacs.d/straight/build-*/tree-sitter-langs/bin/*.dylib; do
   cp -f "$file" ~/.emacs.d/tree-sitter/libtree-sitter-$(basename "$file")
   echo "File: libtree-sitter-$(basename "$file") copied!"
 done
