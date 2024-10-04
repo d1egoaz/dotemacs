cf31cc0f001fe4a7e9db5538a242c3a5fc2d37b0
Diego Alvarez <info@diegoa.ca>
2024-10-04 13:49:08 -0700

    update install method

--- reinstall-emacs.sh
+++ reinstall-emacs.sh
@@ -3,4 +3,5 @@
 brew remove emacs-plus@30
 rm -rf eln-cache
 rm -rf straight/build*
-brew install emacs-plus@30 --with-native-comp --with-savchenkovaleriy-big-sur-curvy-3d-icon
+brew install emacs-plus@30 --with-poll --with-native-comp --with-c9rgreen-sonoma-icon
+rm -rf /Applications/Emacs.app && osascript -e 'tell application "Finder" to make alias file to posix file "/opt/homebrew/opt/emacs-plus@31/Emacs.app" at POSIX file "/Applications" with properties {name:"Emacs.app"}'
